72f5f3ede0845848a7edfbfd341a2162d3bb0d9f
tgnice <tgnice@gmail.com>
2016-04-18 20:55:21 +0900

    initial commit

---
+++ app/js/contents/info.js
@@ -0,0 +1,24 @@
+const userinfo={
+  profile:{
+    name : "SeungHyun Seo",
+    tag : "Computer Scientist",
+    mail : "tgnice@gmail.com",
+    phone : "010 9493 0413",
+    blog : "programming.or.kr",
+    linkedin : "https://kr.linkedin.com/in/seung-hyun-seo-92468335",
+    github : "github.com/tgnice",
+  },
+  educations : [{
+     major : "MSc in Computer Science",
+     school : "Korea Advanced Institute of Science and Technology" ,
+     during : "2011-2013"
+  },{
+     major : "BSc in Computer Science",
+     school : "Hongik University" ,
+     during : "2004-2011"
+  }],
+  languages : [{lang:"English",fluency:"professional"},{lang:"Korean",fluency:"native"}],
+  interests :["Data Science","Computer architecture","Computer Network"],
+  Certification : ["CISA"]
+}
+module.exports= userinfo;
